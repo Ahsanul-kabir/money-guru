93986dc1aadddc882936328a892ac3db027cdd6d
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-02-17 13:59:41 +0600

    balance upper case validation and input clear added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-function getInputValue(moneyAmountId) {
+function getInputValue(moneyAmountId, isClearField) {
     const getAmount = document.getElementById(moneyAmountId);
     const amount = parseFloat(getAmount.value)
+
+    if (isClearField == true) {
+        getAmount.value = ''
+    }
     return amount
 }
 // Take input
@@ -17,33 +21,58 @@ function calculateBalance(incomeValue, foodExpense, rentExpense, clothesExpense)
 }
 
 document.getElementById('calculate-total').addEventListener('click', function () {
-    const incomeValue = getInputValue('income-value')
-    const foodExpense = getInputValue('food-expense')
-    const rentExpense = getInputValue('rent-expense')
-    const clothesExpense = getInputValue('clothes-expense')
+    const incomeValue = getInputValue('income-value', false)
+    const foodExpense = getInputValue('food-expense', false)
+    const rentExpense = getInputValue('rent-expense', false)
+    const clothesExpense = getInputValue('clothes-expense', false)
 
     const allExpense = calculateExpense(foodExpense, rentExpense, clothesExpense)
-    document.getElementById('total-expense').innerText = allExpense
+    const totalExpense = document.getElementById('total-expense')
+    totalExpense.innerText = allExpense
+    totalExpense.style.color = ''
 
     const balance = calculateBalance(incomeValue, foodExpense, rentExpense, clothesExpense)
-    document.getElementById('balance-value').innerText = balance
+    const balanceValue = document.getElementById('balance-value')
+    balanceValue.innerText = balance
+    balanceValue.style.color = ''
 
+    if (allExpense > incomeValue) {
+        alert('Expense More!!')
+        totalExpense.innerText = 'You can not expense more from your income!!!'
+        totalExpense.style.color = 'red'
+        balanceValue.innerText = `${(-balance)}$ money need more!!!`
+        balanceValue.style.color = 'red'
+    }
+    // balance upper case validation
 })
 // Calculate Total Balance
 
 document.getElementById('calculate-save-amount').addEventListener('click', function () {
-    const incomeValue = getInputValue('income-value')
-    const foodExpense = getInputValue('food-expense')
-    const rentExpense = getInputValue('rent-expense')
-    const clothesExpense = getInputValue('clothes-expense')
+    const incomeValue = getInputValue('income-value', true)
+    const foodExpense = getInputValue('food-expense', true)
+    const rentExpense = getInputValue('rent-expense', true)
+    const clothesExpense = getInputValue('clothes-expense', true)
 
     const balance = calculateBalance(incomeValue, foodExpense, rentExpense, clothesExpense)
 
     const savePercentage = getInputValue('save-percentage') // save-percentage
-    const saveAmount = incomeValue * (savePercentage / 100)
-    document.getElementById('save-amount').innerText = saveAmount
+    const saveAmount = (incomeValue * (savePercentage / 100)).toFixed(2)
+    const inputSaveAmount = document.getElementById('save-amount')
+    inputSaveAmount.innerText = saveAmount
+    inputSaveAmount.style.color = ''
 
-    const remainingBalance = balance - saveAmount // remaining-balance
-    document.getElementById('remaining-balance').innerText = remainingBalance
+    const remainingBalance = (balance - saveAmount).toFixed(2) // remaining-balance
+    const inputRemainingBalance = document.getElementById('remaining-balance')
+    inputRemainingBalance.innerText = remainingBalance
+    inputRemainingBalance.style.color = ''
+
+    if (saveAmount > balance) {
+        alert('Saving Not Possible!!')
+        inputSaveAmount.innerText = "You can't save money because if more from your balance!!!"
+        inputSaveAmount.style.color = 'red'
+        inputRemainingBalance.innerText = `At least ${(-remainingBalance)}$ money need more for saving!!!`
+        inputRemainingBalance.style.color = 'red'
+    }
+    // balance upper case validation
 })
 // Calculate Save Amount
